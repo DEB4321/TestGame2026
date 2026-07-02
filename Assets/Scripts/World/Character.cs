256a21dbb2dee9f4e225dbeb4b132ecad9693a51
Davis Brown <45242776+DEB4321@users.noreply.github.com>
2026-07-01 20:41:20 -0400

    Update Character Prefabs

--- Assets/Scripts/World/Character.cs
+++ Assets/Scripts/World/Character.cs
@@ -5,15 +5,23 @@ public class Character : ScriptableObject
 {
     public Sprite sprite;
     public string Name;
+    public int currentHP;
     public int hpStat;
+    public int currentMP;
+    public int MPStat;
     public int strengthStat;
     public int defenseStat;
-    public int magicStat;
+    
     public int magicDefenseStat;
     public int speedStat;
 
     public void LevelUp()
     {
-        
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHP -= damage;
     }
 }
--- Assets/Scripts/World/CharacterUI.cs
+++ Assets/Scripts/World/CharacterUI.cs
@@ -11,9 +11,9 @@ public class CharacterUI : MonoBehaviour
     private Image image;
     private TextMeshProUGUI Name;
     private TextMeshProUGUI HPStat;
+    private TextMeshProUGUI MPStat;
     private TextMeshProUGUI strengthStat;
     private TextMeshProUGUI defenseStat;
-    private TextMeshProUGUI magicStat;
     private TextMeshProUGUI magicDefenseStat;
     private TextMeshProUGUI speedStat;
 
@@ -26,9 +26,9 @@ public class CharacterUI : MonoBehaviour
         TextMeshProUGUI[] text = GetComponentsInChildren<TextMeshProUGUI>();
         Name = text[0];
         HPStat = text[7];
-        strengthStat = text[8];
-        defenseStat = text[9];
-        magicStat = text[10];
+        MPStat = text[8];
+        strengthStat = text[9];
+        defenseStat = text[10];
         magicDefenseStat = text[11];
         speedStat = text[12];
 
@@ -46,10 +46,10 @@ public class CharacterUI : MonoBehaviour
     {
         image.sprite = newCharacter.sprite;
         Name.text = newCharacter.Name;
-        HPStat.text = newCharacter.hpStat.ToString();
+        HPStat.text = $"{newCharacter.currentHP}/{newCharacter.hpStat}";
+        MPStat.text = $"{newCharacter.currentMP}/{newCharacter.MPStat}";
         strengthStat.text = newCharacter.strengthStat.ToString();
         defenseStat.text = newCharacter.defenseStat.ToString();
-        magicStat.text = newCharacter.magicStat.ToString();
         magicDefenseStat.text = newCharacter.magicDefenseStat.ToString();
         speedStat.text = newCharacter.speedStat.ToString();
 
