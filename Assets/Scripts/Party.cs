977e0dc6b421fc9f7474babcf03c8532bde16ddd
Davis Brown <45242776+DEB4321@users.noreply.github.com>
2026-07-24 23:44:55 -0400

    Add Party script

---
+++ Assets/Scripts/Party.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class Party : MonoBehaviour
+{
+    public Character[] currentPartyMembers;
+    public List<Character> remainingCharacters;
+}
